4bb53c398b8ab63c10c8d6d0e54f5f99e7a02616
wayne23123 <wayne23123@gmail.com>
2024-10-02 01:24:45 +0800

    完成floor區域

--- WayneMall/src/views/Home/components/Floor.vue
+++ WayneMall/src/views/Home/components/Floor.vue
@@ -31,11 +31,65 @@
 
       <!-- 下半部 -->
       <div class="floor-info clearfix">
-        <div class="item item1">1</div>
-        <div class="item item2"></div>
-        <div class="item item3"></div>
-        <div class="item item4"></div>
-        <div class="item item5"></div>
+        <div class="item item1">
+          <ul class="item1-list clearfix">
+            <li>
+              <router-link to="/">節能補貼</router-link>
+            </li>
+            <li>
+              <router-link to="/">4k 電視</router-link>
+            </li>
+            <li>
+              <router-link to="/">空氣清淨機</router-link>
+            </li>
+            <li>
+              <router-link to="/">IH 電熱爐</router-link>
+            </li>
+            <li>
+              <router-link to="/">滾筒洗衣機</router-link>
+            </li>
+            <li>
+              <router-link to="/">電熱水器</router-link>
+            </li>
+          </ul>
+          <div class="item1-image-layout">
+            <img
+              src="../../../assets/images/television01.jpg"
+              alt="television02"
+            />
+          </div>
+        </div>
+        <div class="item item2">
+          <img src="../../../assets/images/camera01.jpg" alt="camera01" />
+        </div>
+        <div class="item item3">
+          <div class="item3-image-layout">
+            <img
+              src="../../../assets/images/microwave03.jpg"
+              alt="microwave03"
+            />
+          </div>
+          <div class="item3-image-layout">
+            <img
+              src="../../../assets/images/steamed-rice-box01.jpg"
+              alt="steamed-rice-box01"
+            />
+          </div>
+        </div>
+        <div class="item item4">
+          <img
+            src="../../../assets/images/refrigerator01.jpg"
+            alt="refrigerator01"
+          />
+        </div>
+        <div class="item item5">
+          <div class="item5-image-layout">
+            <img src="../../../assets/images/water01.jpg" alt="water01" />
+          </div>
+          <div class="item5-image-layout">
+            <img src="../../../assets/images/liquor01.jpg" alt="liquor01" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -84,32 +138,114 @@
 }
 
 .floor-info .item1 {
-  width: 230px;
-  height: 432px;
-  background-color: #40f5fc;
+  /* width: 230px; */
+  width: 190px;
+  /* height: 432px; */
+  height: 392px;
+  background-color: #f4f4f4;
+  padding: 20px;
+}
+
+.item1-list {
+  margin-bottom: 29px;
+}
+
+.item1-list li {
+  /* background-color: #f73a3a; */
+  width: 90px;
+  height: 22px;
+  padding-bottom: 3.5px;
+  border-bottom: 1px solid #d9d9d9;
+  font-size: 16px;
+  text-align: center;
+  /* float: left; */
+}
+
+.item1-list li:nth-child(2n-1) {
+  float: left;
+}
+
+.item1-list li:nth-child(2n) {
+  float: right;
+}
+
+.item1-list li:nth-child(3),
+.item1-list li:nth-child(4) {
+  margin: 14.4px 0;
+}
+
+.item1-image-layout {
+  width: 194px;
+  height: 242px;
+}
+
+.item1-image-layout img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 
 .floor-info .item2 {
   width: 340px;
   height: 432px;
-  background-color: #40fc9e;
+  background-color: #fff;
 }
+
+.floor-info .item2 img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
 .floor-info .item3 {
   width: 206px;
   height: 432px;
-  background-color: #c4fc40;
+  background-color: #fff;
   border-right: 1px solid #121212;
 }
+
+.item3-image-layout {
+  width: 206px;
+  height: 215px;
+  border-bottom: 1px solid #121212;
+}
+
+.item3-image-layout img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
 .floor-info .item4 {
   width: 206px;
   height: 432px;
-  background-color: #fca440;
+  background-color: #fff;
   border-right: 1px solid #121212;
+  border-bottom: 1px solid #121212;
 }
+
+.floor-info .item4 img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
 .floor-info .item5 {
   width: 206px;
   height: 432px;
-  background-color: #a740fc;
+  background-color: #fff;
+}
+
+.item5-image-layout {
+  width: 206px;
+  height: 215px;
+  border-bottom: 1px solid #121212;
+}
+
+.item5-image-layout img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 
 /* #endregion */
